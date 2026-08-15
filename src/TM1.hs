5f8154ad2f7eb5824615ba89c595708f8b39e146
cutsea110 <cutsea110@gmail.com>
2026-08-15 09:37:15 +0900

    implement TM1

--- src/TM1.hs
+++ src/TM1.hs
@@ -22,9 +22,40 @@ type Tape = ([Sym], Sym, [Sym])
 initialState :: Q
 initialState = Carry
 
+{- | 加算
+  1. 1 + 1 = 10
+  2. 1 + 0 = 01
+  3. 0 + 1 = 01
+  4. 0 + 0 = 00
+-}
 tm1 :: Program
 tm1 =
   [ ((Carry, One),   (Carry, Zero, Left))
-  , ((Carry, Zero),  (Halt, One, Right))
-  , ((Carry, Blank), (Halt, One, Right))
+  , ((Carry, Zero),  (Halt,  One,  Right))
+  , ((Carry, Blank), (Halt,  One,  Right))
   ]
+
+step :: Program -> (Q, Tape) -> Maybe (Q, Tape)
+step program (state, (ls, h, rs)) =
+  case lookup (state, h) program of
+    Just (newState, newSym, dir) ->
+      Just (newState, move dir (ls, newSym, rs))
+    Nothing -> Nothing
+  where
+    move Left  (l, s, r) = (tl l, hd l, cons (s, r))
+    move Right (l, s, r) = (cons (s, l), hd r, tl r)
+
+    hd []    = Blank
+    hd (x:_) = x
+
+    tl []     = []
+    tl (_:xs) = xs
+
+    cons (Blank, []) = []
+    cons (x, xs)     = x:xs
+
+eval :: Program -> (Q, Tape) -> (Q, Tape)
+eval program (state, tape) =
+  case step program (state, tape) of
+    Just (newState, newTape) -> eval program (newState, newTape)
+    Nothing                  -> (state, tape)
