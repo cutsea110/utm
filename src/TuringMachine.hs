e7a80d7599a2be72337d6c362976bb818ad4d855
cutsea110 <cutsea110@gmail.com>
2026-08-15 09:53:04 +0900

    define TuringMachine class

--- src/TuringMachine.hs
+++ src/TuringMachine.hs
@@ -1,1 +1,13 @@
+{-# LANGUAGE TypeFamilyDependencies #-}
 module TuringMachine where
+
+import qualified UTM
+
+class TuringMachine tm where
+  type State tm
+  type Symbol tm
+
+  initialState :: tm -> State tm
+  blankSymbol  :: tm -> Symbol tm
+
+  transition :: tm -> State tm -> Symbol tm -> Maybe (State tm, Symbol tm, UTM.D)
